07ca3dba511b05c09f0c4a72d59321310bfaa739
MOKGETHOA MOABELO <mokgethoatlhako3@gmail.com>
2026-09-15 12:45:05 +0530

    Add Experiment 1

---
+++ Experiment 1/OrderServices-1/src/test/java/com/klef/OrderServices_1/OrderServices1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klef.OrderServices_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServices1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Experiment 1/RestaurantServices/src/main/java/com/klef/RestaurantServices/RestaurantServicesApplication.java
@@ -0,0 +1,13 @@
+package com.klef.RestaurantServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantServicesApplication.class, args);
+	}
+
+}
